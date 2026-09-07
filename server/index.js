0763ab8de7da5e3de7746b234ec0f7bfe9f81b7b
Vivek Guleria <48416218+jack1-1@users.noreply.github.com>
2026-09-07 11:36:43 +0530

    Update server configuration for environment variables

--- server/index.js
+++ server/index.js
@@ -12,14 +12,14 @@ const router=require('./routes/user.route.js');
 const movieRoutes=require('./routes/movie.route.js')
 app.use(express.json());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:process.env.CLIENT_URL || 'http://localhost:5173',
     credentials:true
 }));
 app.use(cookieParser());
 app.use('/api/auth', router);
 app.use('/api/movie', movieRoutes);
 
-
-app.listen(8001,()=>{
+const PORT = process.env.PORT || 8001;
+app.listen(PORT,()=>{
 console.log("Server is running on port 8001");
 })
